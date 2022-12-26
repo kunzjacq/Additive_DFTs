38eee9d370d38a579933edd72318c0f2f9bf2a86
kunzjacq <kunzjacq@users.noreply.github.com>
2022-12-26 12:00:38 +0100

    mateer-gao product cleanup
    completely remove precomputed tables that were replaced by tables initialized during first use of MG product.

--- libfft/mg.cpp
+++ libfft/mg.cpp
@@ -8,60 +8,34 @@
 
 using namespace std;
 
-#define GRAY_CODE
+//#define GRAY_CODE
 
-/** beta_over_mult_cumulative[i] = sum_j = 0 ... i - 1 beta_i
+/**
+ * @brief beta_over_mult
+ * for i < 63,
+ * beta_over_mult[i] = beta_over_mult[i+1] ^ beta_over_mult[i+1]**2 (*)
+ * where the product is in GF(2**64), in multiplicative representation, i.e. in GF(2**64) as
+ * described in function 'product'
+ * and
+ * beta_over_mult[0] = 1 (**)
+ * .
+ * This table used to be statically generated through another program.
+ * Now it is generated from a known good value beta_over_mult[63] using equation (*) above.
+ * A 'good value' is a value s.t. (**) is satisfied.
+ * 1/2 of all possible starting values are good values, therefore it is not hard to find one.
+ * initialization of beta_over_mult[] and beta_over_mult_cumulative[] is performed by function
+ * init_beta_table().
+ */
+static uint64_t beta_over_mult[64];
+
+static bool beta_is_initialized = false;
+
+/** beta_over_mult_cumulative[i] = sum_{j = 0 ... i - 1} beta_i
  * in multplicative representation, i.e. in GF(2**64) as described in
  * function 'product'. beta_i refers to the beta basis in Mateer-Gao algorithm.
  */
-static constexpr uint64_t beta_over_mult_cumulative_orig[] = {
-  0x0,               0x1,               0x19c9369f278adc03,0xb848d14948d52b96,
-  0xfc39a481a127aa9d,0xfd02a1ae1c3b51c0,0x4ae8fb39198c2000, 0xc9e636090aafc01,
-  0x160266090832ba8e,0x0698fbda5886d236,0x1ed3c0aa2cc028ce,0xa3f2c7d05d050384,
-  0x5ebd73abdf25bd68,0xba04271ef7f3b56a,0x49d80fdbb0b388c2,0xa468c40881568f18,
-  0xf6e2dc67ed2203db,0xf4466dee270e5882,0xf6dc10cf295d7ed4,0x5b7fd08b070abd39,
-  0xa7fe9595d9b22788,0x4738e503d11d72f5,0xb49e3d1bb4227e58,0xf2b4dbb998c79ed7,
-  0xecd3a59c347e16e3,0xe1cfc3d8505a6e77,0xea670e1a534323cf,0x4d4ce92c49c90db4,
-  0xa5b54a92be769797,0x5870d894c2f1596c,0xe69061ed22d5070 ,0xbe658be205cd5c43,
-  0x516e45eea5755655,0xaac030ebd33319f8,0x495ed9b5fabbaf08,0x1695161ab572f7b4,
-  0xadacad2c4e48cdda,0xe45844b9ebae6c40,0x5d36626173c3a040,0xa0264fa8d4864a8e,
-  0x5c071e3bcdce3e37,0x13303c7fe8fafb31,0x07f46edf72e59233,0x029ef3c53091b2d8,
-  0x1ed4dd260daede14,0x153ffd2658c0f389,0xb31cf4415e740389,0x5af7b780a8b08387,
-  0x151dffedf9e1ddbc,0xb79973326bd3fbdf,0x42a28f58bdc05714,0x1fb6289097568c04,
-  0x140379b253de3f29,0x010d3f8d779ee957,0xb33e1fc53acedca3,0xe8fd5ebe929529ba,
-  0xf8387e3f53d81dad,0x4e34099e141594fe,0x15cc6fee9e51daf7,0x0185be136e4c8181,
-  0xb3b50cfd392bc4b1,0xf54ebfb28d2f23cc,0xebeea0f9242d3352,0xe667293b4ee307b4,
-  0x6d1861bfc6fbe3e8
-};
 
-#ifdef GRAY_CODE
-static constexpr uint64_t beta_over_mult_orig[] = {
-  0x1               , 0x19c9369f278adc02, 0xa181e7d66f5ff795, 0x447175c8e9f2810b,
-  0x013b052fbd1cfb5d, 0xb7ea5a9705b771c0, 0x467698598926dc01, 0x1a9c05699898468f,
-  0x109a9dd350b468b8, 0x184b3b707446faf8, 0xbd21077a71c52b4a, 0xfd4fb47b8220beec,
-  0xe4b954b528d60802, 0xf3dc28c547403da8, 0xedb0cbd331e507da, 0x528a186f6c748cc3,
-  0x2a4b189ca2c5b59,  0x029a7d210e532656, 0xada3c0442e57c3ed, 0xfc81451edeb89ab1,
-  0xe0c6709608af557d, 0xf3a6d818653f0cad, 0x462ae6a22ce5e08f, 0x1e677e25acb98834,
-  0x0d1c664464247894, 0x0ba8cdc203194db8, 0xa72be7361a8a2e7b, 0xe8f9a3bef7bf9a23,
-  0xfdc592067c87cefb, 0x5619de8a10dc091c, 0xb00c8dfcd7e00c33, 0xef0bce0ca0b80a16,
-  0xfbae750576464fad, 0xe39ee95e2988b6f0, 0x5fcbcfaf4fc958bc, 0xbb39bb36fb3a3a6e,
-  0x49f4e995a5e6a19a, 0xb96e26d8986dcc00, 0xfd102dc9a745eace, 0xfc215193194874b9,
-  0x4f3722442534c506, 0x14c452a09a1f6902, 0x056a9d1a427420eb, 0x1c4a2ee33d3f6ccc,
-  0x0beb2000556e2d9d, 0xa623096706b4f000, 0xe9eb43c1f6c4800e, 0x4fea486d51515e3b,
-  0xa2848cdf92322663, 0xf53bfc6ad613accb, 0x5d14a7c82a96db10, 0x0bb55122c488b32d,
-  0x150e463f2440d67e, 0xb23320484d5035f4, 0x5bc3417ba85bf519, 0x10c52081c14d3417,
-  0xb60c77a147cd8953, 0x5bf866708a444e09, 0x1449d1fdf01d5b76, 0xb230b2ee57674530,
-  0x46fbb34fb404e77d, 0x1ea01f4ba902109e, 0x0d8989c26ace34e6, 0x8b7f48848818e45c
-};
-
-#endif
-
-static uint64_t beta_over_mult_alt[64];
-static uint64_t beta_over_mult_cumulative_alt[64];
-
-static uint64_t* beta_over_mult = beta_over_mult_alt;
-static uint64_t* beta_over_mult_cumulative = beta_over_mult_cumulative_alt;
-
+static uint64_t beta_over_mult_cumulative[64];
 
 void naive_product(uint64_t* p1u, uint64_t n1, uint64_t* p2u, uint64_t n2, uint64_t* qu)
 {
@@ -157,40 +131,54 @@ static void contract(uint64_t* dest, uint64_t* source, uint64_t d)
 static inline uint64_t product(const uint64_t&a, const uint64_t&b)
 {
   constexpr uint64_t minpoly = 0x1b; // x**64 = x**4 + x**3 + x + 1
-  // x**64 + x**4 + x**3 + x + 1 is primitive over GF(2)
-  // it is the minimal polynomial of the multiplicative generator
+  // p = x**64 + x**4 + x**3 + x + 1 is primitive over GF(2)
+  // it is the minimal polynomial of the multiplicative generator g
+  // which therefore satisfies g**64 + g**4 + g**3 + g + 1
   __m128i xa, xb, xc;
-  xa = _mm_set_epi64x(minpoly, a);
-  xb = _mm_set1_epi64x(b);
-  xb = _mm_clmulepi64_si128(xa, xb, 0x00);
-  xc = _mm_clmulepi64_si128(xa, xb, 0x11);
-  xb = _mm_xor_si128(xb, xc);
-  xc = _mm_clmulepi64_si128(xa, xc, 0x11);
-  xb = _mm_xor_si128(xb, xc);
-  return _mm_extract_epi64(xb, 0);
+  xa = _mm_set_epi64x(minpoly, a); // xa_0 = a, xa_1 = minpoly
+  xb = _mm_set1_epi64x(b); // xb_0 = b
+  // below, * is the carry-less multiplication of 64-bit values which produces a 128-bit value
+  // and which is implemented by the instruction PCLMUL in the amd64 instruction set
+  // if 128-bit words are interpreted as polynomials evaluated at g, * and × coincide, but *
+  // produces values that are larger than 64 bits and that must therefore be reduced mod p
+  xb = _mm_clmulepi64_si128(xa, xb, 0x00); // xb = xa_0 * xb_0 = a * b
+  xc = _mm_clmulepi64_si128(xa, xb, 0x11); // xc = xb_1 * minpoly
+  // g**64 × xb_1 = minpoly × xb_1 as values in GF(2**64) (since p(g) = g**64 ^ minpoly = 0)
+  // minpoly * xb_1 is a 68-bit value
+  xb = _mm_xor_si128(xb, xc); // xb = xb ^ xc (the result is accumulated in xb_0)
+  xc = _mm_clmulepi64_si128(xa, xc, 0x11); // xc = xc_1 * minpoly
+  // g**64 × xc_1 = minpoly × xc_1  as values in GF(2**64)
+  // minpoly * xc_1 is a 8-bit value, therefore after computatin, xc_1 = 0
+  // and the result is xb_0 ^ xc_0
+  xb = _mm_xor_si128(xb, xc); // xb = xb ^ xc
+  return _mm_extract_epi64(xb, 0); // return xb_0
 }
 
-#include<iostream>
-#include<iomanip>
-
-void init_alt_table(){
-  //constexpr uint64_t start_value = 0x8b7f48848818e45c;//0x2000000000000000; // does not work with the second value for some reason
-  constexpr uint64_t start_value = 0x8000000000000001;
-  // tested to generate the full suite
-  beta_over_mult_alt[63] = start_value;
-  for(int i = 62; i >= 0; i--) {
-    uint64_t val = beta_over_mult_alt[i+1];
-    beta_over_mult_alt[i] = val^product(val, val);
+int init_beta_table()
+{
+  //constexpr uint64_t start_value = 0x8b7f48848818e45c; // start value previously used in table generated externally
+  constexpr uint64_t start_value = 0x8000000000000000;
+  beta_over_mult[63] = start_value;
+  int iter = 0;
+  do
+  {
+    beta_over_mult[63]++;
+    for(int i = 62; i >= 0; i--)
+    {
+      uint64_t val = beta_over_mult[i+1];
+      beta_over_mult[i] = val^product(val, val);
+    }
+    iter++;
   }
+  while(beta_over_mult[0] != 1);
 
-  beta_over_mult_cumulative_alt[0]=0;
-  for(int i = 1; i < 64; i++) {
-    beta_over_mult_cumulative_alt[i]=beta_over_mult_cumulative_alt[i-1]^beta_over_mult_alt[i-1];
+  beta_over_mult_cumulative[0] = 0;
+  for(int i = 1; i < 64; i++)
+  {
+    beta_over_mult_cumulative[i] = beta_over_mult_cumulative[i-1]^beta_over_mult[i-1];
   }
-
-  if(beta_over_mult_alt[0] != 1) {
-    std::cout << "***************Error in array initialization!!*************" << std::endl;
-  }
+  beta_is_initialized = true;
+  return iter;
 }
 
 /**
@@ -223,10 +211,12 @@ static void product_batch(uint64_t* restr a_ptru, uint64_t* restr b_ptru, unsign
     xb1 = _mm_load_si128(b_ptr128);
     xb3 = _mm_load_si128(b_ptr128 + 1);
 #if 1
-    xa2 = _mm_shuffle_epi32(xa1, 0x4e); // put higher part of xa1 in xa2
-    xa4 = _mm_shuffle_epi32(xa3, 0x4e); // (dwords 3, 2, in lower part, 3*4+2 = e,
-    xb2 = _mm_shuffle_epi32(xb1, 0x4e); // 1, 0 in higher part, 1 * 4 + 0 = 4)
-    xb4 = _mm_shuffle_epi32(xb3, 0x4e);
+    xa2 = _mm_shuffle_epi32(xa1, 0x4e); // put higher part of xa1 in lower part of xa2:
+    // dwords 3, 2 in lower part, 3*4+2 = e,
+    // dwords 1, 0 in higher part, 1 * 4 + 0 = 4
+    xa4 = _mm_shuffle_epi32(xa3, 0x4e); // same with xa3 / xa4
+    xb2 = _mm_shuffle_epi32(xb1, 0x4e); // same with xb1 / xb2
+    xb4 = _mm_shuffle_epi32(xb3, 0x4e); // same with xb3 / xb4
 #else
     xa2 = _mm_unpackhi_epi64(xa1, xb2); //xa1 = upper half of xa2; xb2 unused here
     xa4 = _mm_unpackhi_epi64(xa3, xb2); //xb2 unused here
@@ -556,6 +546,7 @@ void mg_reverse_core(
 
 void mg_binary_polynomial_multiply(uint64_t* p1, uint64_t* p2, uint64_t* result, uint64_t d1, uint64_t d2)
 {
+  if(!beta_is_initialized) init_beta_table();
   constexpr unsigned int s = 6;
   uint64_t offsets_mult[1 << s];
   for(int i = 0; i < (1 << s); i++) offsets_mult[i] = 0;
@@ -593,6 +584,7 @@ void mg_binary_polynomial_multiply(uint64_t* p1, uint64_t* p2, uint64_t* result,
 
 void mg_binary_polynomial_multiply_in_place (uint64_t* p1, uint64_t* p2, uint64_t d1, uint64_t d2)
 {
+  if(!beta_is_initialized) init_beta_table();
   constexpr unsigned int s = 6;
   uint64_t offsets_mult[1 << s];
   for(int i = 0; i < (1 << s); i++) offsets_mult[i] = 0;
